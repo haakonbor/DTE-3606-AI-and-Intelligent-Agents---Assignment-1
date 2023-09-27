9c60f0cf4d98b47ba44b323a9a572b8e7f79ea51
Håkon Berg Borhaug <hbo038@post.uit.no>
2023-09-27 14:37:01 +0200

    Started adjusting Q-learning algorithm for non-deterministic cases

--- main.py
+++ main.py
@@ -37,14 +37,20 @@ class QLearning:
         self.r_table = np.zeros((10, 10))
         self.q_table = np.zeros((self.state_space, self.action_space))
 
+        # Probability of gaining reward
+        self.reward_prob = np.zeros((10, 10))
+
         # Terminal state 1: -500
         self.r_table[0][8] -= 500
+        self.reward_prob[0][8] = 1
 
         # Terminal state 2: +1000
         self.r_table[5][9] += 1000
+        self.reward_prob[5][9] = 1
 
         # Terminal state 3: -100
         self.r_table[8][6] -= 100
+        self.reward_prob[8][6] = 1
 
         self.rewards = np.zeros(self.episodes)
         self.steps = np.zeros(self.episodes)
@@ -86,9 +92,11 @@ class QLearning:
     def in_terminal_state(self, state):
         return state in self.terminal_states
 
+    def reward_func(self, new_state, movement_cost):
+        return self.r_table[new_state[0], new_state[1]] + movement_cost
+
     def learn(self):
         for episode in range(self.episodes):
-            # Start in a random state in either row "A" or "I", and before column 4.
             state = [0, 0]
 
             actions_made = []
@@ -111,15 +119,15 @@ class QLearning:
                 states_visited.append(state)
 
                 movement_cost = 0 if action == Action.WAIT else -5
-                reward = self.r_table[new_state[0], new_state[1]] + movement_cost
+                reward = self.reward_func(new_state, movement_cost)
+                reward_prob = self.reward_prob[new_state[0], new_state[1]]
                 self.rewards[episode] += reward
                 self.steps[episode] += movement_cost / -5
 
                 prev_q = self.q_table[q_index, action]
 
-                # Chapter 6.5 in "Reinforcement Learning - An Introduction, 2nd edition"
-                # by Richard S. Sutton and Andrew G. Barto
-                td_error = reward + self.discount_factor * max(self.q_table[new_q_index]) - prev_q
+                # Q-learning algorithm
+                td_error = reward_prob * reward + self.discount_factor * max(self.q_table[new_q_index]) - prev_q
                 self.abs_td_error[episode] += abs(td_error)
                 q = prev_q + self.learning_rate * td_error
                 self.q_table[q_index, action] = q
@@ -177,7 +185,7 @@ class QLearning:
         axs[2].set_ylabel('Error')
         axs[2].set_title(f'Running average absolute TD error (window = {avg_range})')
 
-        fig.set_size_inches(10, 10)
+        fig.set_size_inches(8, 8)
         plt.subplots_adjust(hspace=0.5)
         plt.show()
 
@@ -193,18 +201,18 @@ class QLearningBountyHunter(QLearning):
         for terminal_state in self.terminal_states:
             self.r_table[terminal_state] = 0
         self.hideouts = [[5, 1], [6, 8]]
-        self.hideouts_prob = [0.25, 0.75]
+        self.hideouts_prob = [0.35, 0.65]
+        for i in range(len(self.hideouts)):
+            self.r_table[self.hideouts[i][0], self.hideouts[i][1]] = 1000
+            self.reward_prob[self.hideouts[i][0], self.hideouts[i][1]] = self.hideouts_prob[i]
         self.thief_pos = self.hideouts[0]
-        self.r_table[self.thief_pos[0], self.thief_pos[1]] = 1000
         self.terminal_states.append(self.thief_pos)
 
     def move_thief(self):
         for hideout in self.hideouts:
-            self.r_table[hideout] = 0
-
+            self.r_table[hideout[0], hideout[1]] = 0
         # Thief picks to move or not with given probability
         self.thief_pos = self.hideouts[np.random.choice(len(self.hideouts), p=self.hideouts_prob)]
-
         self.r_table[self.thief_pos[0], self.thief_pos[1]] = 1000
         self.terminal_states[0] = self.thief_pos
 
@@ -242,13 +250,14 @@ class QLearningBountyHunterWithAssistant(QLearningBountyHunter):
                 states_visited[0].append(states[0])
 
                 movement_cost = 0 if actions[0] == Action.WAIT else -5
-                reward = self.r_table[new_states[0][0], new_states[0][1]] + movement_cost
+                reward = self.reward_func(new_states[0], movement_cost)
+                reward_prob = self.reward_prob[new_states[0][0], new_states[0][1]]
                 self.rewards[episode] += reward
                 self.steps[episode][0] += movement_cost / -5
 
                 prev_q = self.q_table[q_indexes[0], actions[0]]
 
-                td_error = reward + self.discount_factor * max(self.q_table[new_q_indexes[0]]) - prev_q
+                td_error = reward_prob * reward + self.discount_factor * max(self.q_table[new_q_indexes[0]]) - prev_q
                 self.abs_td_error[episode] += abs(td_error)
                 q = prev_q + self.learning_rate * td_error
                 self.q_table[q_indexes[0], actions[0]] = q
@@ -266,13 +275,14 @@ class QLearningBountyHunterWithAssistant(QLearningBountyHunter):
                 states_visited[1].append(states[1])
 
                 movement_cost = 0 if actions[1] == Action.WAIT else -5
-                reward = self.r_table[new_states[1][0], new_states[1][1]] + movement_cost
+                reward = self.reward_func(new_states[1], movement_cost)
+                reward_prob = self.reward_prob[new_states[1][0], new_states[1][1]]
                 self.rewards[episode] += reward
                 self.steps[episode][1] += movement_cost / -5
 
                 prev_q = self.q_table[q_indexes[1], actions[1]]
 
-                td_error = reward + self.discount_factor * max(self.q_table[new_q_indexes[1]]) - prev_q
+                td_error = reward_prob * reward + self.discount_factor * max(self.q_table[new_q_indexes[1]]) - prev_q
                 self.abs_td_error[episode] += abs(td_error)
                 q = prev_q + self.learning_rate * td_error
                 self.q_table[q_indexes[1], actions[1]] = q
